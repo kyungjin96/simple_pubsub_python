9a2acf834086e8b5a8e6092c136bdaee4e50295b
kyungjin96 <wjsrudwls96@naver.com>
2022-01-10 14:47:53 +0900

    커밋

--- simple_pubsub_python/basic_subscriber.py
+++ simple_pubsub_python/basic_subscriber.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 class BasicSubscriber(Node):
+    
     def __init__(self):
         super().__init__('basic_subscriber')
         self.subscription = self.create_subscription(
@@ -10,8 +11,10 @@ class BasicSubscriber(Node):
             self.listener_callback,
             10)
         self.subscription
+
     def listener_callback(self, msg):
         self.get_logger().info('Received: "%s"' % msg.data)
+
 def main(args=None):
     rclpy.init(args=args)
     basic_subcriber = BasicSubscriber()
